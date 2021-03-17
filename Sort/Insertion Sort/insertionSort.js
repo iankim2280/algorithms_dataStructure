58b8cd4fab84766231b17e6ddf0f784824b98cf8
iankim2280 <iankim2280@gmail.com>
2021-03-17 14:52:19 -0400

    var to let

--- Sort/Insertion Sort/insertionSort.js
+++ Sort/Insertion Sort/insertionSort.js
@@ -1,9 +1,9 @@
 const insertionSort = (arr) => {
-  var currentVal;
-  for (var i = 1; i < arr.length; i++) {
+  let currentVal;
+  for (let i = 1; i < arr.length; i++) {
     currentVal = arr[i];
     // cause you compare to the previous number, use let j = i - 1 and it should be greater than index 0
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = currentVal;
